000e260018626335f3141611bdfa1709460dcab5
pragati-sacumen <pragati.gupta@sacumen.com>
2023-04-27 14:57:46 +0530

    a new Function added

--- first.py
+++ first.py
@@ -6,5 +6,6 @@ def greet(a = "Employee Name"):
     return f"Hi {a}, Welcome to Team "
 print(greet("Pragati"))
 
-
-
+def reverse(a="name"):
+    return a[::-1]
+print(reverse(greet("esrever drow ")))
