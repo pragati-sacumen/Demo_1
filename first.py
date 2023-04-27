84ad3611b974beb14a210ca6061950e0605a825c
pragati-sacumen <pragati.gupta@sacumen.com>
2023-04-27 15:51:27 +0530

    added a counter function

--- first.py
+++ first.py
@@ -10,3 +10,15 @@ def reverse(a="name"):
     return a[::-1]
 print(reverse(greet("esrever drow ")))
 
+def counter(l):
+    d = dict()
+    c=1
+    for i in l:
+        if i not in d:
+            d[i]=c
+        else :
+            d[i]+=1
+    return d
+l = [1,2,3,4,5,6,1,3,3,2,4,5]
+print(counter(l))
+        
